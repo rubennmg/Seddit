5828851c2d1aa1479cd6374df47d9ca97e80d924
rubennmg <rubenmg2015@gmail.com>
2024-02-20 10:55:53 +0100

    implemented user authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  ## Devise: Set default URL options for the Devise mailer
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 } # TODO: Change this to your domain
 end
