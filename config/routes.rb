95b79fc0336e385716984d7b9335a37617afc996
rubennmg <rubenmg2015@gmail.com>
2024-02-21 10:05:21 +0100

    showing user posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :communities
   devise_for :users
   get 'home/index'
+  get 'home/user_posts'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "home#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
